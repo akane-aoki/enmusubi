f931518378f476e84f7262049ababbbcc3e35859
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-01-15 22:46:37 +0900

    Fix: routesにresources追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to events_path, success: 'ユーザー登録が完了しました'
+      redirect_to _path, success: 'ユーザー登録が完了しました'
     else
       flash.now[:danger] = 'ユーザー登録に失敗しました'
       render :new
