3af06d1f4d086ad862f5e7258b0749fbfbe3da81
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-01-16 22:54:00 +0900

    Add: postモデル、コントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_052340) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_133736) do
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.date "date"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -23,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_052340) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
