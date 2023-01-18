adbedd76ada53cb5602da81c640b344435699bd2
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-01-18 20:17:01 +0900

    Add: effortモデル作成

---
+++ db/migrate/20230118111332_create_efforts.rb
@@ -0,0 +1,10 @@
+class CreateEfforts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :efforts do |t|
+      t.string :body, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
