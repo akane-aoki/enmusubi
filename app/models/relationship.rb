9608e7db1605e56cde07c42514c5972ce9debc93
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-01-18 20:39:18 +0900

    Fix: relationモデル修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,5 @@
 class Relationship < ApplicationRecord
-  has_many :users, dependent: :destroy
+  has_many :users
   has_one :meet, dependent: :destroy
   has_one :reward, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_111332) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_112012) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "efforts", force: :cascade do |t|
     t.string "body", null: false
     t.integer "user_id", null: false
@@ -57,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_111332) do
     t.index ["relationship_id"], name: "index_users_on_relationship_id"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "efforts", "users"
   add_foreign_key "meets", "relationships"
   add_foreign_key "posts", "users"
