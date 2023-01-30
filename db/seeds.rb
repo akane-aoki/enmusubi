7e554f996090bdbea6933092230c8d11143c6921
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-01-30 21:10:11 +0900

    Fix: seedsに全てのモデル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# relationship
+2.times do
+  Relationship.create
+end
+
 # user
 2.times do
   User.create(
@@ -17,8 +22,18 @@
   )
 end
 
+2.times do
+  User.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'password',
+      password_confirmation: 'password',
+      relationship_id: 2
+  )
+end
+
 # post
-3.times do |n|
+4.times do |n|
   body = Faker::Creature::Dog.breed
   date = Faker::Date.in_date_period
   user = User.find(n + 1)
@@ -26,3 +41,22 @@ end
                 date: date,
                 user: user)
 end
+
+# meet
+Meet.create(
+    meet_day: Faker::Date.between(from: 30.days.ago, to: Date.today),
+    relationship_id: 1
+)
+
+# reward
+Reward.create(
+  not_meet_day: 30,
+  content: "ユニバ！",
+  relationship_id: 1
+)
+
+# efforts
+Efforts.create(
+  body: ,"筋トレ"
+  user_id: 1
+)
