d602f1e4272e7813b9dbe9a673829058c08fd18d
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-01-31 20:00:35 +0900

    Add: tailwind,daisyui入れ直し

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#8C0327",
+          "secondary": "#D85251",
+          "accent": "#D59B6A",
+          "neutral": "#826A5C",
+          "base-100": "#F1F1F1",
+          "info": "#42ADBB",
+          "success": "#499380",
+          "warning": "#E97F14",
+          "error": "#DF1A2F",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
 }
