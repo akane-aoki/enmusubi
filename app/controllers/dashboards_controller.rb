dbbcc46ffacc003d2969cc4eba5659236563875b
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-02-01 20:21:43 +0900

    meetsのseedsデータ変更

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,10 +10,15 @@ class DashboardsController < ApplicationController
     # @meet = @user.meet.find(params[:id])
 
     # @meet = Meet.where(relationship_id: current_user.relationship_id).order(meet_day: :desc).limit(1).pluck(:meet_day)
+
+    # meetsモデル
     today = Date.current
     meet_arr = Meet.where(relationship_id: current_user.relationship_id).order(meet_day: :desc).limit(1)
     @meet = meet_arr.pluck(:meet_day).first
 
     @day_count = (@meet - today).to_i
+
+    # rewardsモデル
+    @my_reward = Reward.where(relationship_id: current_user.relationship_id).pluck(:content)
   end
 end
--- db/migrate/05_create_rewards.rb
+++ db/migrate/05_create_rewards.rb
@@ -2,7 +2,7 @@ class CreateRewards < ActiveRecord::Migration[7.0]
   def change
     create_table :rewards do |t|
       t.integer :not_meet_day, null: false
-      t.string :content, null: false
+      t.string :content, null: false, unique: true
       t.references :relationship, null: false, foreign_key: true
 
       t.timestamps
