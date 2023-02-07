b551cffde7aba5af6c82a559373242642c63ce2a
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-02-07 20:56:12 +0900

    Add: userモデルにgeocoding関係追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, uniqueness: true, presence: true
 
+  geocoded_by :address #geocodingを行いたいカラムを指定
+  after_validation :geocode, if: :address_changed? #geocodingするタイミングを指定
+
   def own?(object)
     id == object.user_id
   end
