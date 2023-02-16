d631cfad27cf303b42fffa2c4a8e6f9135ac307f
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-02-16 22:40:45 +0900

    Add: topページのjs実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,16 +1,32 @@
-console.log('a')
+import jquery from "jquery"
+window.$ = jquery
 
-function previewFileWithId(id) {
-  console.log('bbb');
-  const target = this.event.target;
-  const file = target.files[0];
-  const reader  = new FileReader();
-  reader.onloadend = function () {
-      img_prev.src = reader.result;
+// function previewFileWithId(img_prev) {
+//   const target = this.event.target;
+//   const file = target.files[0];
+//   const reader  = new FileReader();
+//   reader.onloadend = function () {
+//     img_prev.src = reader.result;
+//   }
+//   if (file) {
+//     reader.readAsDataURL(file);
+//   } else {
+//     img_prev.src = '';
+//   }
+// }
+{/* <script type="text/javascript"> */}
+$(function() {
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+  $('#img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+      }
   }
-  if (file) {
-      reader.readAsDataURL(file);
-  } else {
-      img_prev.src = '';
-  }
-}
+  $("#board_img").change(function(){
+      readURL(this);
+  });
+});
+// </script>
