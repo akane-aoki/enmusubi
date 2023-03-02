e01d15c6e83648e40b17634a3260f35787a6afa5
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-03-02 23:00:37 +0900

    Fix: sitemap.rbを修正

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,26 +2,9 @@
 SitemapGenerator::Sitemap.default_host = "https://enkyori-enmusubi.com"
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  add root_path, :changefreq => 'daily', :priority => 1.0
+  # ログイン
+  add login_path, :priority => 0.5
+  # ユーザー登録
+  add new_user_path, :priority => 0.6
 end
