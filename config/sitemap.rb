e514ff5b2bc1852a12c3fae41bba29fd48f26d7e
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-03-03 21:01:23 +0900

    Add: S3の環境変数設定

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,12 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://enkyori-enmusubi.com"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['AWS_BUCKET_NAME']}"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  Rails.application.credentials[:aws][:bucket_name],
+  aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
+  aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
+  aws_region: Rails.application.credentials[:aws][:region],
+)
 
 SitemapGenerator::Sitemap.create do
   add root_path, :changefreq => 'daily', :priority => 1.0
