9c8006450fbd5be2c32b593133537992bb37637f
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-03-04 22:44:14 +0900

    Fix: opg修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
       og: {
         separator: '|',
         site_name: '遠結び',
-        title: '遠距離恋愛の「寂しい」を「楽しい」に。',
+        title: :full_title, # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
         description: '遠距離恋愛中で、恋人と会えない寂しさを抱えている方に、会えない時間を充実させ、楽しみに変えてくれるサービスです。恋人と一緒に使って、会えない時間も楽しもう！',
         type: 'website',
         url: request.original_url,
