212d744ba02e506feb8b6ed30ea669bd7220ae7c
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-03-04 21:50:27 +0900

    Add: headerロゴ追加とfavicon変更

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,3 +1,5 @@
+require 'aws-sdk-s3'
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://enkyori-enmusubi.com"
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
