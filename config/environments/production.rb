0043780f9bf6c8d419f49760f59038b90069f415
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-03-10 18:51:24 +0900

    Fix: productionの設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "enmusubi_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
